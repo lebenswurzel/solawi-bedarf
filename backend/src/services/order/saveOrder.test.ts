9f81d11b4b750ba7b7aec8184e1249c9007e9cb9
joergrs <joergschroeter84@gmail.com>
2024-07-06 16:49:57 +0200

    saveOrder test with actual products; saving empty order #52

--- backend/src/services/order/saveOrder.test.ts
+++ backend/src/services/order/saveOrder.test.ts
@@ -22,6 +22,16 @@ import {
 } from "../../../testSetup";
 import { saveOrder } from "./saveOrder";
 import { getOrder } from "./getOrder";
+import { ConfirmedOrder } from "../../../../shared/src/types";
+import { UserCategory } from "../../../../shared/src/enum";
+import {
+  findOrdersByUser,
+  getDepotByName,
+  getProductByName,
+  updateRequisition,
+} from "../../test/testHelpers";
+import { assert } from "console";
+import { bi } from "../bi/bi";
 
 test("prevent unauthorized access", async () => {
   const ctx = createBasicTestCtx();
@@ -39,3 +49,106 @@ testAsUser1(
     await expect(() => getOrder(ctx)).rejects.toThrowError("Error 403");
   },
 );
+
+testAsUser1("save empty order", async ({ userData }: TestUserData) => {
+  await updateRequisition(true);
+  const depot = await getDepotByName("d1");
+  const request: ConfirmedOrder = {
+    confirmGTC: true,
+    category: UserCategory.CAT100,
+    categoryReason: "nothing special",
+    depotId: depot.id,
+    orderItems: [],
+    offer: 0,
+    alternateDepotId: null,
+    offerReason: null,
+    validFrom: null,
+  };
+
+  // create order
+  const ctx = createBasicTestCtx(request, userData.token, undefined, {
+    id: userData.userId,
+  });
+
+  expect((await findOrdersByUser(userData.userId)).length).toBe(0);
+
+  await saveOrder(ctx);
+  expect(ctx.status).toBe(204);
+  const orders = await findOrdersByUser(userData.userId);
+  expect(orders.length).toBe(1);
+  expect(orders[0].categoryReason).toBe("nothing special");
+  expect(orders[0].userId).toBe(userData.userId);
+
+  // update order
+  const ctx2 = createBasicTestCtx(
+    { ...request, categoryReason: "updated" },
+    userData.token,
+    undefined,
+    {
+      id: userData.userId,
+    },
+  );
+  await saveOrder(ctx2);
+  expect(ctx2.status).toBe(204);
+  const orders2 = await findOrdersByUser(userData.userId);
+  expect(orders2.length).toBe(1);
+  expect(orders2[0].categoryReason).toBe("updated");
+  expect(orders[0].userId).toBe(userData.userId);
+});
+
+testAsUser1("save order with products", async ({ userData }: TestUserData) => {
+  await updateRequisition(true);
+  const depot = await getDepotByName("d1");
+  const baseRequest: ConfirmedOrder = {
+    confirmGTC: true,
+    category: UserCategory.CAT100,
+    categoryReason: "nothing special",
+    depotId: depot.id,
+    orderItems: [],
+    offer: 0,
+    alternateDepotId: null,
+    offerReason: null,
+    validFrom: null,
+  };
+
+  const product1 = await getProductByName("p1");
+  const product2 = await getProductByName("p2");
+
+  const orderItem1 = {
+    productId: product1.id,
+    value: 3,
+  };
+  const orderItem2 = {
+    productId: product2.id,
+    value: 2,
+  };
+  const request = {
+    ...baseRequest,
+    offer: 100,
+    orderItems: [orderItem1, orderItem2],
+  };
+
+  // create order
+  const ctx = createBasicTestCtx(request, userData.token, undefined, {
+    id: userData.userId,
+  });
+
+  // run twice to ensure idempotency
+  for (let i = 0; i < 2; i++) {
+    await saveOrder(ctx);
+
+    expect(ctx.status).toBe(204);
+    const orders = await findOrdersByUser(userData.userId);
+    expect(orders.length).toBe(1);
+    expect(orders[0].categoryReason).toBe("nothing special");
+    expect(orders[0].userId).toBe(userData.userId);
+    expect(orders[0].orderItems).toMatchObject([orderItem1, orderItem2]);
+
+    const { soldByProductId, capacityByDepotId, productsById } = await bi();
+    expect(soldByProductId[product1.id].sold).toBe(3);
+    expect(soldByProductId[product2.id].sold).toBe(2);
+    expect(capacityByDepotId[depot.id].reserved).toBe(1);
+    expect(productsById[product1.id]).toMatchObject(product1);
+    expect(productsById[product2.id]).toMatchObject(product2);
+  }
+});
---
+++ backend/src/test/testHelpers.ts
@@ -0,0 +1,120 @@
+/*
+This file is part of the SoLawi Bedarf app
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+import {
+  RequisitionConfig,
+  RequisitionConfigName,
+} from "../database/RequisitionConfig";
+import { AppDataSource } from "../database/database";
+import { Depot } from "../database/Depot";
+import { Order } from "../database/Order";
+import { Product } from "../database/Product";
+import { Unit } from "../../../shared/src/enum";
+import { ProductCategory } from "../database/ProductCategory";
+import { OrderItem } from "../database/OrderItem";
+
+export const fillDatabaseWithTestData = async () => {
+  const products = ["p1", "p2", "p3", "p4"];
+  const depots = ["d1", "d2", "d3"];
+
+  for (const depot of depots) {
+    await createTestDepot(depot);
+  }
+  for (const product of products) {
+    await createTestProduct(product);
+  }
+};
+
+const createTestDepot = async (name: string) => {
+  AppDataSource.getRepository(Depot).save({
+    name,
+    address: `${name} address`,
+    openingHours: "9-5",
+    capacity: 12,
+    active: true,
+  });
+};
+
+const createTestProduct = async (name: string) => {
+  const pcEntity = await AppDataSource.getRepository(ProductCategory).save({
+    name: `${name} category`,
+    active: true,
+  });
+  await AppDataSource.getRepository(Product).save({
+    name,
+    description: `description of product ${name}`,
+    active: true,
+    msrp: 10,
+    quantity: 10,
+    quantityMin: 1,
+    quantityMax: 5,
+    quantityStep: 1,
+    unit: Unit.PIECE,
+    orderItems: [],
+    productCategoryId: pcEntity.id,
+  });
+};
+
+export const updateRequisition = async (
+  biddingOpen: boolean,
+  requistionName?: string,
+) => {
+  const now = new Date();
+  requistionName = requistionName || RequisitionConfigName;
+  const repo = AppDataSource.getRepository(RequisitionConfig);
+  const requisition = await repo.findOneByOrFail({ name: requistionName });
+
+  if (biddingOpen) {
+    // bidding from yesterday until tomorrow
+    requisition.startOrder = dateDeltaDays(-1);
+    requisition.endBiddingRound = dateDeltaDays(1);
+  } else {
+    // bidding round in the past
+    requisition.startOrder = dateDeltaDays(-2);
+    requisition.endBiddingRound = dateDeltaDays(-1);
+  }
+  await repo.save(requisition);
+};
+
+const dateDeltaDays = (deltaDays: number): Date => {
+  return new Date(Date.now() + deltaDays * 24 * 60 * 60 * 1000);
+};
+
+export const getDepots = async (): Promise<Depot[]> => {
+  return AppDataSource.getRepository(Depot).find();
+};
+
+export const getDepotByName = async (name: string): Promise<Depot> => {
+  const depots = await getDepots();
+  return depots.filter((v) => v.name == name)[0];
+};
+
+export const findOrdersByUser = async (userId: number) => {
+  const orders = await AppDataSource.getRepository(Order).findBy({
+    userId,
+  });
+  for (const order of orders) {
+    const orderItems = await AppDataSource.getRepository(OrderItem).findBy({
+      orderId: order.id,
+    });
+    order.orderItems = orderItems;
+  }
+  return orders;
+};
+
+export const getProductByName = async (name: string): Promise<Product> => {
+  return await AppDataSource.getRepository(Product).findOneByOrFail({ name });
+};
