4022b74acd102fdc68ddbcf3a25d8168578cdcb2
joergrs <joergschroeter84@gmail.com>
2024-07-08 20:35:21 +0200

    100% test coverage saveDepot and getDepot #23

--- apply_license_headers.bash
+++ apply_license_headers.bash
@@ -7,10 +7,11 @@
 
 APP_NAME="the SoLawi Bedarf app"
 
-EXCLUDES='/node_modules/|/dist/|vite-env.d.ts|vfs_fonts.ts|eslintrc'
+EXCLUDES='/node_modules/|/dist/|vite-env.d.ts|vfs_fonts.ts|eslintrc|backend/coverage'
 FOLDERS='\./backend|\./frontend|\./shared/src'
 
-MODIFIED_FILE=0
+MODIFIED_FILES=0
+COUNT=0
 
 while read -r FILE; do
   if [ ! -f "$FILE" ]; then
@@ -18,14 +19,17 @@ while read -r FILE; do
     exit 1
   fi
 
+  ((COUNT=COUNT+1))
   APP_NAME="$APP_NAME" python3 dev/apply_license_headers.py "$FILE" "$@"
 
   if [ "$?" != "0" ]; then
-    MODIFIED_FILE=1
+    ((MODIFIED_FILES=MODIFIED_FILES+1))
   fi
 done < <(find . -type f -name '*.ts' -o -name '*.js' -o -name '*.vue' | grep -E "$FOLDERS" | grep -v -E "$EXCLUDES")
 
 if [ "$1" == "--check" ]; then
-  echo $MODIFIED_FILE
-  exit $MODIFIED_FILE
+  echo "$MODIFIED_FILES of $COUNT files need header fixing"
+  exit $MODIFIED_FILES
 fi
+
+echo "Found $COUNT files"
