8d6b916a4fbeb3734d700574bc4636d8dbeeccc8
joergrs <joergschroeter84@gmail.com>
2024-08-21 22:19:32 +0200

    make backups folder writable for group postgres #53

--- dev/backup/database-backup.bash
+++ dev/backup/database-backup.bash
@@ -3,9 +3,18 @@
 DATABASE=solawi
 CONTAINER=solawi-bedarf-db-1
 NOW=$(date --iso-8601=seconds)
-TARGET_PATH=/backups/$DATABASE_$NOW.sql.gz
+BACKUPS_DIR=/backups
+TARGET_PATH=$BACKUPS_DIR/$DATABASE_$NOW.sql.gz
 
 echo "Backing up database $DATABASE to file $TARGET_PATH (in the container)"
+
+echo "Making $BACKUPS_DIR writable for group 'postgres' to allow writing the backup"
+docker exec -t $CONTAINER bash -c "chown :postgres $BACKUPS_DIR && chmod g+w $BACKUPS_DIR"
+if [ $? -ne 0 ]; then
+  echo "Error: unable to set writable permission to $BACKUPS_DIR"
+  exit 1
+fi
+
 docker exec --user postgres -t $CONTAINER bash -c "pg_dump $DATABASE | gzip > $TARGET_PATH"
 if [ $? -ne 0 ]; then
   echo "Error: Failed to create a database backup."
