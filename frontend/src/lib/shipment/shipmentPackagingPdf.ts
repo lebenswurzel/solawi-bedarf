61f5255f6024554458ddb29f686213daa7cb8a11
Richard Liebscher <r1tschy@posteo.de>
2024-09-12 22:31:12 +0200

    refactor: add functions for reused functionality #39

--- frontend/src/lib/shipment/shipmentPackagingPdf.ts
+++ frontend/src/lib/shipment/shipmentPackagingPdf.ts
@@ -26,36 +26,84 @@ import { multiplicatorOptions } from "../options";
 import { format } from "date-fns";
 import { generatePdf } from "../pdf/pdf";
 import { sanitizeFileName } from "../../../../shared/src/util/fileHelper";
+import { TCreatedPdf } from "pdfmake/build/pdfmake";
 
-export const createShipmentPackagingPdfs = async (
+function getOrCompute<V>(obj: Record<string, V>, key: string, fn: (k: string) => V): V {
+  const value = obj[key];
+  if (value === undefined) {
+    const init = fn(key);
+    obj[key] = init;
+    return init;
+  } else {
+    return value;
+  }
+}
+
+class Zip {
+  private jszip: JSZip;
+
+  constructor() {
+    this.jszip = new JSZip();
+  }
+
+  public async addPdf(pdf: TCreatedPdf, filename: string) {
+    const blob: Blob = await new Promise((resolve, _) => {
+      pdf.getBlob((blob) => resolve(blob));
+    });
+    this.jszip.file(filename, blob, { binary: true });
+  }
+
+  public download(filename: string) {
+    this.jszip.generateAsync({ type: "blob" }).then((content) => {
+      const blob = new Blob([content], { type: "zip" });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    });
+  }
+}
+
+function findDepotById(depots: Depot[], depotId: number): Depot {
+  return depots.find((d) => d.id == depotId);
+}
+
+function findDepotNameById(depots: Depot[], depotId: number): string {
+  return findDepotById(depots, depotId)?.name || "Unbekanntes Depot";
+}
+
+interface Product {
+  Bezeichnung: string;
+  Einheit: string;
+  Menge: number;
+  Bemerkung: string;
+}
+
+type GroupedProducts = Record<string, Product[]>;
+type DepotGroupedProducts = Record<string, GroupedProducts>;
+
+export async function createShipmentPackagingPdfs(
   shipment: Shipment,
   depots: Depot[],
   productsById: ProductsById,
-  productCategories: ProductCategoryWithProducts[],
-) => {
-  const dataByDepotAndProductCategory: {
-    [key: string]: {
-      [key: string]: {
-        Bezeichnung: string;
-        Einheit: string;
-        Menge: number;
-        Bemerkung: string;
-      }[];
-    };
-  } = {};
+  productCategories: ProductCategoryWithProducts[]
+) {
+  const dataByDepotAndProductCategory: DepotGroupedProducts = {};
   for (let item of shipment.shipmentItems) {
-    const depot =
-      depots.find((d) => d.id == item.depotId)?.name || "Unbekanntes Depot";
+    const depot = findDepotNameById(depots, item.depotId);
     const product = productsById[item.productId];
     const productCategory =
       productCategories.find((pc) => pc.id == product.productCategoryId)
         ?.name || "Unbekannte Kategorie";
-    if (!dataByDepotAndProductCategory[depot]) {
-      dataByDepotAndProductCategory[depot] = {};
-    }
-    if (!dataByDepotAndProductCategory[depot][productCategory]) {
-      dataByDepotAndProductCategory[depot][productCategory] = [];
-    }
+
+    const groupedProducts = getOrCompute(dataByDepotAndProductCategory, depot, () => {
+      return {};
+    });
+    const products = getOrCompute(groupedProducts, productCategory, () => []);
     const multiplicator =
       item.multiplicator != 100
         ? multiplicatorOptions.find((mo) => mo.value == item.multiplicator)
@@ -66,7 +114,7 @@ export const createShipmentPackagingPdfs = async (
         ? `(${item.conversionFrom} ${getLangUnit(product.unit)} -> ${item.conversionTo} ${getLangUnit(item.unit)})`
         : "";
     const description = item.description ? item.description : "";
-    dataByDepotAndProductCategory[depot][productCategory].push({
+    products.push({
       Bezeichnung: `${product.name}${item.isBio ? " [BIO]" : ""}`,
       Einheit: getLangUnit(item.unit),
       Menge: item.totalShipedQuantity,
@@ -75,27 +123,23 @@ export const createShipmentPackagingPdfs = async (
   }
   for (let item of shipment.additionalShipmentItems) {
     const productCategory = "Zusätzliches Angebot";
-    const depot =
-      depots.find((d) => d.id == item.depotId)?.name || "Unbekanntes Depot";
-    if (!dataByDepotAndProductCategory[depot]) {
-      dataByDepotAndProductCategory[depot] = {};
-    }
-    if (!dataByDepotAndProductCategory[depot][productCategory]) {
-      dataByDepotAndProductCategory[depot][productCategory] = [];
-    }
+    const depot = findDepotNameById(depots, item.depotId);
+    const groupedProducts = getOrCompute(dataByDepotAndProductCategory, depot, () => {
+      return {};
+    });
+    const products = getOrCompute(groupedProducts, productCategory, () => []);
     const description = item.description ? item.description : "";
-    dataByDepotAndProductCategory[depot][productCategory].push({
+    products.push({
       Bezeichnung: `${item.product}${item.isBio ? " [BIO]" : ""}`,
       Einheit: getLangUnit(item.unit),
       Menge: item.totalShipedQuantity,
       Bemerkung: `${description}`,
     });
   }
-  const zip = new JSZip();
-  const depotKeys = Object.keys(dataByDepotAndProductCategory);
+
   const prettyDate = format(shipment.validFrom, "dd.MM.yyyy");
-  for (let depotKey of depotKeys) {
-    const dataByProductCategory = dataByDepotAndProductCategory[depotKey];
+  const zip = new Zip();
+  for (const [depotKey, dataByProductCategory] of Object.entries(dataByDepotAndProductCategory)) {
     let description = `Lieferschein für ${prettyDate}`;
     if (shipment.description) {
       description += `\n\n${shipment.description}`;
@@ -107,20 +151,8 @@ export const createShipmentPackagingPdfs = async (
       `Depot ${depotKey}`,
       `Lieferschein ${prettyDate}`,
     );
-    const blob: Blob = await new Promise((resolve, _) => {
-      pdf.getBlob((blob) => resolve(blob));
-    });
-    zip.file(`${sanitizeFileName(depotKey)}.pdf`, blob, { binary: true });
+    zip.addPdf(pdf, `${sanitizeFileName(depotKey)}.pdf`);
   }
-  zip.generateAsync({ type: "blob" }).then((content) => {
-    const blob = new Blob([content], { type: "zip" });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `shipments-${format(shipment.validFrom, "yyyy-MM-dd")}.zip`;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  });
-};
+
+  zip.download(`shipments-${format(shipment.validFrom, "yyyy-MM-dd")}.zip`);
+}
