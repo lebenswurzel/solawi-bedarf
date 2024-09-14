770ed520dfb1b9558f009d9b47b3b5cbbafd3ac9
Richard Liebscher <r1tschy@posteo.de>
2024-09-14 21:58:27 +0200

    Add tests #39

---
+++ frontend/src/lib/overview.test.ts
@@ -0,0 +1,364 @@
+/*
+This file is part of the SoLawi Bedarf app
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { describe, expect, test } from "vitest";
+import {
+  generateDepotData,
+  generateUserData,
+  OverviewItem,
+} from "./overview.ts";
+import { Product } from "../../../shared/src/types.ts";
+import { UserCategory } from "../../../shared/src/enum.ts";
+import { PdfTable } from "./pdf/pdf.ts";
+import {
+  CUCUMBER,
+  genProduct,
+  genProductGroupWithProducts,
+  MILK,
+  MILK_PRODUCTS,
+  TOMATO,
+  VEGETABLES,
+} from "../../testSetup.ts";
+
+function genOrder(overwrite: Partial<OverviewItem> = {}): OverviewItem {
+  const base: OverviewItem = {
+    name: "Order",
+    depot: "Main depot",
+    alternateDepot: "Alt depot",
+    msrp: 0,
+    offer: 100,
+    offerReason: "",
+    category: UserCategory.CAT115,
+    categoryReason: "Just do it :)",
+    items: [],
+  };
+  return { ...base, ...overwrite };
+}
+
+function genOrderItem(product: Product, value: number) {
+  return {
+    name: product.name,
+    value,
+    unit: product.unit,
+    category: product.productCategoryId,
+  };
+}
+
+// test generateUserData
+describe("summarize demand by user", () => {
+  test("for single order", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const cucumber = genProduct(CUCUMBER);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato, cucumber],
+    });
+
+    const milk = genProduct(MILK);
+    const mil_products = genProductGroupWithProducts({
+      name: MILK_PRODUCTS.name,
+      products: [milk],
+    });
+
+    let productCategories = [vegetables, mil_products];
+
+    let orders = [
+      genOrder({
+        name: "Order",
+        depot: "Main depot",
+        items: [
+          genOrderItem(tomato, 200),
+          genOrderItem(cucumber, 3),
+          genOrderItem(milk, 3000),
+        ],
+      }),
+    ];
+
+    // ACT
+    let actual = generateUserData(orders, productCategories);
+
+    // ASSERT
+    let pdf = actual.get("Order\nMain depot");
+    expect(pdf).not.toBeUndefined();
+    expect([...pdf!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [
+        [CUCUMBER.name, "3 Stk.", "20 x"],
+        [TOMATO.name, "200 g", "30 x"],
+      ],
+    } as PdfTable);
+    expect(pdf!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "3000 ml", "56 x"]],
+    } as PdfTable);
+  });
+
+  test("for multiple orders", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const cucumber = genProduct(CUCUMBER);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato, cucumber],
+    });
+
+    const milk = genProduct(MILK);
+    const mil_products = genProductGroupWithProducts({
+      name: MILK_PRODUCTS.name,
+      products: [milk],
+    });
+
+    let productCategories = [vegetables, mil_products];
+
+    let orders = [
+      genOrder({
+        name: "Order1",
+        depot: "Main depot",
+        items: [genOrderItem(tomato, 200), genOrderItem(milk, 3000)],
+      }),
+      genOrder({
+        name: "Order2",
+        depot: "Main depot",
+        items: [genOrderItem(cucumber, 3), genOrderItem(milk, 1000)],
+      }),
+    ];
+
+    // ACT
+    let actual = generateUserData(orders, productCategories);
+
+    // ASSERT
+    let pdf1 = actual.get("Order1\nMain depot");
+    expect(pdf1).not.toBeUndefined();
+    expect([...pdf1!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf1!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[TOMATO.name, "200 g", "30 x"]],
+    } as PdfTable);
+    expect(pdf1!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "3000 ml", "56 x"]],
+    } as PdfTable);
+
+    let pdf2 = actual.get("Order2\nMain depot");
+    expect(pdf2).not.toBeUndefined();
+    expect([...pdf1!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf2!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[CUCUMBER.name, "3 Stk.", "20 x"]],
+    } as PdfTable);
+    expect(pdf2!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "1000 ml", "56 x"]],
+    } as PdfTable);
+  });
+
+  test("while ignoring empty items", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato],
+    });
+
+    let productCategories = [vegetables];
+
+    let orders = [
+      genOrder({
+        name: "Order",
+        depot: "Main depot",
+        items: [genOrderItem(tomato, 0)],
+      }),
+    ];
+
+    // ACT
+    let actual = generateUserData(orders, productCategories);
+
+    // ASSERT
+    expect(actual.size).toBe(0);
+  });
+});
+
+// test generateDepotData
+describe("summarize demand by depot", () => {
+  test("for single depot", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const cucumber = genProduct(CUCUMBER);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato, cucumber],
+    });
+
+    const milk = genProduct(MILK);
+    const mil_products = genProductGroupWithProducts({
+      name: MILK_PRODUCTS.name,
+      products: [milk],
+    });
+
+    let productCategories = [vegetables, mil_products];
+
+    let orders = [
+      genOrder({
+        name: "1",
+        depot: "Main depot",
+        items: [
+          genOrderItem(tomato, 200),
+          genOrderItem(cucumber, 3),
+          genOrderItem(milk, 3000),
+        ],
+      }),
+      genOrder({
+        name: "2",
+        depot: "Main depot",
+        items: [
+          genOrderItem(tomato, 100),
+          genOrderItem(cucumber, 2),
+          genOrderItem(milk, 7000),
+        ],
+      }),
+    ];
+
+    // ACT
+    let actual = generateDepotData(orders, productCategories);
+
+    // ASSERT
+    let pdf = actual.get("Main depot");
+    expect(pdf).not.toBeUndefined();
+    expect([...pdf!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [
+        [CUCUMBER.name, "5 Stk.", "20 x"],
+        [TOMATO.name, "300 g", "30 x"],
+      ],
+    } as PdfTable);
+    expect(pdf!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "10000 ml", "56 x"]],
+    } as PdfTable);
+  });
+
+  test("for multiple depots", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const cucumber = genProduct(CUCUMBER);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato, cucumber],
+    });
+
+    const milk = genProduct(MILK);
+    const mil_products = genProductGroupWithProducts({
+      name: MILK_PRODUCTS.name,
+      products: [milk],
+    });
+
+    let productCategories = [vegetables, mil_products];
+
+    let orders = [
+      genOrder({
+        name: "1",
+        depot: "Main depot",
+        items: [genOrderItem(tomato, 200), genOrderItem(milk, 3000)],
+      }),
+      genOrder({
+        name: "2",
+        depot: "Alt depot",
+        items: [genOrderItem(cucumber, 3), genOrderItem(milk, 1000)],
+      }),
+    ];
+
+    // ACT
+    let actual = generateDepotData(orders, productCategories);
+
+    // ASSERT
+    let pdf1 = actual.get("Main depot");
+    expect(pdf1).not.toBeUndefined();
+    expect([...pdf1!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf1!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[TOMATO.name, "200 g", "30 x"]],
+    } as PdfTable);
+    expect(pdf1!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "3000 ml", "56 x"]],
+    } as PdfTable);
+
+    let pdf2 = actual.get("Alt depot");
+    expect(pdf2).not.toBeUndefined();
+    expect([...pdf1!.keys()]).toEqual([VEGETABLES.name, MILK_PRODUCTS.name]);
+    expect(pdf2!.get(VEGETABLES.name)).toStrictEqual({
+      name: VEGETABLES.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[CUCUMBER.name, "3 Stk.", "20 x"]],
+    } as PdfTable);
+    expect(pdf2!.get(MILK_PRODUCTS.name)).toStrictEqual({
+      name: MILK_PRODUCTS.name,
+      headers: ["Bezeichnung", "Menge", "geplante Häufigkeit"],
+      widths: ["70%", "10%", "20%"],
+      rows: [[MILK.name, "1000 ml", "56 x"]],
+    } as PdfTable);
+  });
+
+  test("while ignoring empty items", () => {
+    // ARRANGE
+    const tomato = genProduct(TOMATO);
+    const vegetables = genProductGroupWithProducts({
+      name: VEGETABLES.name,
+      products: [tomato],
+    });
+
+    let productCategories = [vegetables];
+
+    let orders = [
+      genOrder({
+        name: "Order",
+        depot: "Main depot",
+        items: [genOrderItem(tomato, 0)],
+      }),
+    ];
+
+    // ACT
+    let actual = generateDepotData(orders, productCategories);
+
+    // ASSERT
+    expect(actual.size).toBe(0);
+  });
+});
