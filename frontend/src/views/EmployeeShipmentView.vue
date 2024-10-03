1fb37c2cbb7206c707734ee9711c310b1f33c162
joergrs <joergschroeter84@gmail.com>
2024-10-03 22:59:07 +0200

    using watch instead of $subscribe to react to config changes #23

--- frontend/src/views/EmployeeShipmentView.vue
+++ frontend/src/views/EmployeeShipmentView.vue
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script setup lang="ts">
-import { onMounted, provide, ref } from "vue";
+import { onMounted, provide, ref, watch } from "vue";
 import { language } from "../lang/lang.ts";
 import { useBIStore } from "../store/biStore.ts";
 import ShipmentDialog from "../components/ShipmentDialog.vue";
@@ -38,6 +38,7 @@ import {
   Shipment,
 } from "../../../shared/src/types.ts";
 import { getShipments } from "../requests/shipment.ts";
+import { storeToRefs } from "pinia";
 
 const t = language.pages.shipment;
 
@@ -54,6 +55,8 @@ const shipments = ref<(Shipment & Id)[]>([]);
 const biStore = useBIStore();
 const configStore = useConfigStore();
 const productStore = useProductStore();
+
+const { activeConfigId } = storeToRefs(configStore);
 
 const shipment = ref<EditShipment & OptionalId>(defaultEditShipment);
 const savedShipment = ref<Shipment & Id>();
@@ -99,17 +102,19 @@ const onEditShipment = (dialogShipment: Shipment & Id) => {
 
 const onClose = async () => {
   open.value = false;
-  await biStore.update(configStore.activeConfigId);
+  await biStore.update(activeConfigId.value);
   shipments.value = (await getShipments()).shipments;
 };
 
 const refresh = async () => {
-  await productStore.update(configStore.activeConfigId);
-  await biStore.update(configStore.activeConfigId);
+  await productStore.update(activeConfigId.value);
+  await biStore.update(activeConfigId.value);
   shipments.value = (await getShipments()).shipments;
 };
 onMounted(refresh);
-configStore.$subscribe(refresh);
+watch(activeConfigId, async () => {
+  refresh();
+});
 </script>
 
 <template>
