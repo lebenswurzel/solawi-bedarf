4a0dddd383f98f8a79200b4154329cbaba83f536
joergrs <joergschroeter84@gmail.com>
2024-10-05 14:25:40 +0200

    form validation #23

--- frontend/src/components/NewSeasonDialog.vue
+++ frontend/src/components/NewSeasonDialog.vue
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 -->
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useConfigStore } from "../store/configStore";
 import { language } from "../lang/lang";
 import { dateToString, stringToDate } from "../lib/convert";
@@ -34,17 +34,40 @@ const newSeasonStartDate = ref<Date>(
   new Date(new Date().getFullYear() + 1, 3, 1),
 );
 const copyFromSeasonId = ref<number | undefined>();
+const isValid = ref<boolean>(false);
 const onConfirmCreate = () => {
-  copyFromSeasonId.value = undefined;
-  newSeasonName.value = "";
+  if (!isValid.value) {
+    return;
+  }
   console.log(
     "CREATE",
     copyFromSeasonId.value,
     newSeasonName.value,
     newSeasonStartDate.value,
   );
+
   onClose();
 };
+
+watch(configStore, () => {
+  copyFromSeasonId.value = configStore.activeConfigId;
+});
+onMounted(() => {
+  copyFromSeasonId.value = configStore.activeConfigId;
+});
+
+const newSeasonNameRules = [
+  (value: string) => {
+    if (!value) return "Name darf nicht leer sein";
+    return true;
+  },
+  (value: string) => {
+    if (configStore.availableConfigs.map((v) => v.name).includes(value)) {
+      return "Es existiert bereits eine Saison mit dieser Bezeichnung";
+    }
+    return true;
+  },
+];
 
 const configOptions = computed(() => {
   return configStore.availableConfigs.map((config) => ({
@@ -55,35 +78,38 @@ const configOptions = computed(() => {
 </script>
 <template>
   <v-dialog :model-value="open" @update:model-value="onClose">
-    <v-card>
-      <v-card-title>{{ t.newSeason.title }}</v-card-title>
-      <v-card-text>
-        <v-text-field
-          :label="t.name"
-          type="text"
-          v-model="newSeasonName"
-        ></v-text-field>
-        <v-select
-          :label="t.newSeason.copyFromPrevious"
-          :items="configOptions"
-          v-model="copyFromSeasonId"
-        ></v-select>
-        <v-text-field
-          :label="t.validFrom"
-          type="datetime-local"
-          :model-value="dateToString(newSeasonStartDate)"
-          @update:model-value="
-            (val: string) =>
-              (newSeasonStartDate = stringToDate(val) || newSeasonStartDate)
-          "
-        ></v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn @click="onConfirmCreate">{{ language.app.actions.apply }}</v-btn>
-        <v-btn color="error" @click="onClose">{{
-          language.app.actions.cancel
-        }}</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-form v-model="isValid" @submit="onConfirmCreate" @submit.prevent>
+      <v-card>
+        <v-card-title>{{ t.newSeason.title }}</v-card-title>
+        <v-card-text>
+          <v-text-field
+            :label="t.name"
+            :rules="newSeasonNameRules"
+            type="text"
+            v-model="newSeasonName"
+          ></v-text-field>
+          <v-select
+            :label="t.newSeason.copyFromPrevious"
+            :items="configOptions"
+            v-model="copyFromSeasonId"
+          ></v-select>
+          <v-text-field
+            :label="t.validFrom"
+            type="datetime-local"
+            :model-value="dateToString(newSeasonStartDate)"
+            @update:model-value="
+              (val: string) =>
+                (newSeasonStartDate = stringToDate(val) || newSeasonStartDate)
+            "
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn type="submit">{{ language.app.actions.apply }}</v-btn>
+          <v-btn color="error" @click="onClose">{{
+            language.app.actions.cancel
+          }}</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
   </v-dialog>
 </template>
