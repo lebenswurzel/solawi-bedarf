6bff504454ecabbecdcb5717b6ee7c845c688e0d
joergrs <joergschroeter84@gmail.com>
2024-10-11 22:12:22 +0200

    new database field: RequisitionConfig.public #23

---
+++ backend/src/migrations/1728677345133-config-public-flag.ts
@@ -0,0 +1,32 @@
+/*
+This file is part of the SoLawi Bedarf app
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class ConfigPublicFlag1728677345133 implements MigrationInterface {
+    name = 'ConfigPublicFlag1728677345133'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "requisition_config" ADD "public" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "depot" ALTER COLUMN "rank" SET NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "depot" ALTER COLUMN "rank" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "requisition_config" DROP COLUMN "public"`);
+    }
+
+}
