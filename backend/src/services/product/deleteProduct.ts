0a426f2ff4f7383b00dd2039321a896956249113
joergrs <joergschroeter84@gmail.com>
2024-10-21 22:43:56 +0200

    deleting a product #23

---
+++ backend/src/services/product/deleteProduct.ts
@@ -0,0 +1,73 @@
+/*
+This file is part of the SoLawi Bedarf app
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+import Koa from "koa";
+import Router from "koa-router";
+import { UserRole } from "../../../../shared/src/enum";
+import { Id } from "../../../../shared/src/types";
+import { http } from "../../consts/http";
+import { AppDataSource } from "../../database/database";
+import { OrderItem } from "../../database/OrderItem";
+import { Product } from "../../database/Product";
+import { ShipmentItem } from "../../database/ShipmentItem";
+import { getUserFromContext } from "../getUserFromContext";
+
+export const deleteProduct = async (
+  ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>, any>,
+) => {
+  const { role } = await getUserFromContext(ctx);
+  if (role != UserRole.ADMIN) {
+    ctx.throw(http.forbidden);
+  }
+  const product = ctx.request.body as Id;
+  const existing = await AppDataSource.getRepository(Product).findOneBy({
+    id: product.id || -1,
+  });
+  if (!existing) {
+    ctx.throw(http.bad_request, `product id=${product.id} does not exist`);
+  }
+
+  // check existing orders
+  const order = await AppDataSource.getRepository(OrderItem).findOne({
+    where: {
+      productId: product.id,
+    },
+  });
+  if (order) {
+    ctx.throw(
+      http.bad_request,
+      `product ${existing.name} id=${existing.id} is part of an order`,
+    );
+  }
+
+  // check existing shipments
+  const shipmentItem = await AppDataSource.getRepository(ShipmentItem).findOne({
+    where: {
+      productId: product.id,
+    },
+  });
+  if (shipmentItem) {
+    ctx.throw(
+      http.bad_request,
+      `product ${existing.name} id=${existing.id} is part of a shipment`,
+    );
+  }
+
+  // delete product
+  await AppDataSource.getRepository(Product).delete({ id: product.id });
+
+  ctx.status = http.no_content;
+};
