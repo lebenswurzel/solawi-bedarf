b93d32e2a86e3571db3a5234790420e36899d7f6
joergrs <joergschroeter84@gmail.com>
2024-11-08 21:25:34 +0100

    show stats below shipment info #135

--- frontend/src/components/styled/SeasonText.vue
+++ frontend/src/components/styled/SeasonText.vue
@@ -17,13 +17,22 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useConfigStore } from "../../store/configStore";
+import { computed } from "vue";
+
+const props = defineProps<{
+  plain?: boolean;
+}>();
 
 const configStore = useConfigStore();
 const { config, seasonColorClass } = storeToRefs(configStore);
+
+const colorClass = computed(() => {
+  return props.plain ? "" : seasonColorClass.value;
+});
 </script>
 
 <template>
-  <span :class="seasonColorClass" class="season_text">
+  <span :class="colorClass" class="season_text">
     <v-icon v-if="!config?.public" size="x-small">mdi-eye-off-outline</v-icon>
     {{ config?.name || "SAISON??" }}</span
   >
