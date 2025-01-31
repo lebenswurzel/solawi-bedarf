546908cc060634d55e8eeac8bdf7e4bc1c0aba96
joergrs <joergschroeter84@gmail.com>
2025-01-31 12:20:23 +0100

    fix test #142

--- backend/src/services/text/saveTextContent.test.ts
+++ backend/src/services/text/saveTextContent.test.ts
@@ -481,7 +481,9 @@ const validateTextContentsWithoutOrgInfo = async (
   const ctx = createBasicTestCtx(undefined, userData.token);
   await getTextContent(ctx);
   const filteredTexts = ctx.body.textContent.filter(
-    (t: TextContent) => t.category != TextContentCategory.ORGANIZATION_INFO,
+    (t: TextContent) =>
+      t.category != TextContentCategory.ORGANIZATION_INFO &&
+      t.category != TextContentCategory.PDF,
   );
   expect(new Set(filteredTexts.map((v: TextContent) => v.content))).toEqual(
     new Set(expectedTexts),
