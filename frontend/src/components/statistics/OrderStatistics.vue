59e80f53e2f4300328a28972091362f1a4a855c3
joergrs <joergschroeter84@gmail.com>
2025-02-01 10:53:33 +0100

    show distributions as bar plots #174

--- frontend/src/components/statistics/OrderStatistics.vue
+++ frontend/src/components/statistics/OrderStatistics.vue
@@ -25,6 +25,7 @@ import { UserCategory } from "../../../../shared/src/enum.ts";
 import { storeToRefs } from "pinia";
 import { format } from "date-fns";
 import { de } from "date-fns/locale";
+import DistributionPlot, { DistributionData } from "./DistributionPlot.vue";
 
 const t = language.pages.statistics;
 const userStore = useUserStore();
@@ -85,65 +86,87 @@ onMounted(async () => {
   isProcessing.value = false;
 });
 
-const categoryCounts = computed(() => {
-  return orders.value.reduce(
+const categoriesDistribution = computed((): DistributionData => {
+  const items = orders.value.reduce(
     (acc, cur) => {
       return acc.map((c) => {
-        if (c.title == cur.category) {
-          return { ...c, count: c.count + 1 };
+        if (c.label == cur.category) {
+          return { ...c, value: c.value + 1 };
         } else {
           return c;
         }
       });
     },
     [
-      { title: UserCategory.CAT100, count: 0 },
-      { title: UserCategory.CAT115, count: 0 },
-      { title: UserCategory.CAT130, count: 0 },
+      { label: UserCategory.CAT100, value: 0 },
+      { label: UserCategory.CAT115, value: 0 },
+      { label: UserCategory.CAT130, value: 0 },
     ],
   );
+  return {
+    items: items.map((i) => ({
+      ...i,
+      label:
+        language.app.options.orderUserCategories[i.label].title +
+        ` (${i.label})`,
+    })),
+  };
 });
 
-const offerRanges = computed(() => {
-  return orders.value.reduce(
-    (acc, cur) => {
-      return acc.map((c) => {
-        if (cur.offer >= c.offerMin && cur.offer < c.offerMax) {
-          return { ...c, count: c.count + 1 };
-        } else {
-          return c;
-        }
-      });
-    },
-    [
-      { offerMin: 0, offerMax: 50, count: 0 },
-      { offerMin: 50, offerMax: 100, count: 0 },
-      { offerMin: 100, offerMax: 150, count: 0 },
-      { offerMin: 150, offerMax: 200, count: 0 },
-      { offerMin: 200, offerMax: 250, count: 0 },
-      { offerMin: 250, offerMax: 500, count: 0 },
-      { offerMin: 500, offerMax: 1000, count: 0 },
-    ],
-  );
-});
-const depotDistribution = computed(() => {
-  return orders.value
-    .reduce(
-      (acc, order) => {
+const offerRanges = computed(
+  (): { data: DistributionData; offersSum: number } => {
+    const items = orders.value.reduce(
+      (acc, cur) => {
         return acc.map((c) => {
-          if (c.depotName == order.depotName) {
+          if (cur.offer >= c.offerMin && cur.offer < c.offerMax) {
             return { ...c, count: c.count + 1 };
           } else {
             return c;
           }
         });
       },
-      depots.value.map((d) => ({
-        depotName: d.name,
-        count: 0,
-      })),
-    )
-    .sort((a, b) => (a.depotName < b.depotName ? -1 : 1));
+      [
+        { offerMin: 0, offerMax: 50, count: 0 },
+        { offerMin: 50, offerMax: 100, count: 0 },
+        { offerMin: 100, offerMax: 150, count: 0 },
+        { offerMin: 150, offerMax: 200, count: 0 },
+        { offerMin: 200, offerMax: 250, count: 0 },
+        { offerMin: 250, offerMax: 500, count: 0 },
+        { offerMin: 500, offerMax: 1000, count: 0 },
+      ],
+    );
+    return {
+      data: {
+        items: items.map((i) => ({
+          label: `${i.offerMin} € < x < ${i.offerMax} €`,
+          value: i.count,
+        })),
+      },
+      offersSum: orders.value.reduce((acc, cur) => acc + cur.offer, 0),
+    };
+  },
+);
+
+const depotDistribution = computed((): DistributionData => {
+  return {
+    items: orders.value
+      .reduce(
+        (acc, order) => {
+          return acc.map((c) => {
+            if (c.label == order.depotName) {
+              return { ...c, value: c.value + 1 };
+            } else {
+              return c;
+            }
+          });
+        },
+        depots.value.map((d) => ({
+          label: d.name,
+          value: 0,
+        })),
+      )
+      .sort((a, b) => (a.label < b.label ? -1 : 1)),
+  };
 });
 </script>
 
@@ -197,63 +220,18 @@ const depotDistribution = computed(() => {
       <v-container fluid>
         <v-row>
           <v-col cols="12" sm="6" lg="4">
-            <v-table>
-              <thead>
-                <tr>
-                  <th>Kategorie</th>
-                  <th>Anzahl</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="catCount in categoryCounts">
-                  <td>
-                    {{
-                      language.app.options.orderUserCategories[catCount.title]
-                        .title
-                    }}
-                    ({{ catCount.title }})
-                  </td>
-                  <td>{{ catCount.count }}</td>
-                </tr>
-              </tbody>
-            </v-table>
+            <div class="text-subtitle-1">Mitgliedschaftsmodell</div>
+            <DistributionPlot :distribution-data="categoriesDistribution" />
           </v-col>
 
           <v-col cols="12" sm="6" lg="4">
-            <v-table>
-              <thead>
-                <tr>
-                  <th>Monatsbeitrag</th>
-                  <th>Anzahl</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="range in offerRanges">
-                  <td>
-                    {{ range.offerMin }} € &le; x &lt; {{ range.offerMax }} €
-                  </td>
-                  <td>{{ range.count }}</td>
-                </tr>
-              </tbody>
-            </v-table>
+            <div class="text-subtitle-1">Monatsbeitrag</div>
+            <div>Summe: {{ offerRanges.offersSum }} €</div>
+            <DistributionPlot :distribution-data="offerRanges.data" />
           </v-col>
           <v-col cols="12" sm="6" lg="4">
-            <v-table>
-              <thead>
-                <tr>
-                  <th>Depot</th>
-                  <th>Anzahl</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="depotCount in depotDistribution">
-                  <td>
-                    {{ depotCount.depotName }}
-                  </td>
-                  <td>{{ depotCount.count }}</td>
-                </tr>
-              </tbody>
-            </v-table>
+            <div class="text-subtitle-1">Depot-Belegung</div>
+            <DistributionPlot :distribution-data="depotDistribution" />
           </v-col>
         </v-row>
       </v-container>
