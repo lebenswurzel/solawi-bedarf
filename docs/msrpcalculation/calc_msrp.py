3bb92368cfba021d2bb281ae18234e71df90be79
joergrs <joergschroeter84@gmail.com>
2025-11-15 13:03:59 +0100

    iterative calculation #302

--- docs/msrpcalculation/calc_msrp.py
+++ docs/msrpcalculation/calc_msrp.py
@@ -13,59 +13,68 @@ class Product:
 class MsrpSeasonPart:
     product: Product
     amount: float  # amount of units of this product
-    msrp_eff: float | None  # effective msrp for this product
     months: int  # number of months of start of order until end of season
     weight: (
         float  # remaining amount of the product in the season when the msrp was calculated (0..1)
     )
 
+    monthly_due_effective: float | None = None
+    effective_months: int | None = None
+    effective_weight: float | None = None
+    overpay: float = 0
+
     def get_total_msrp(self) -> float:
         return self.product.base_msrp * self.product.frequency * self.amount
 
+    def set_monthly_due_effective(self, amount: float):
+        self.monthly_due_effective = amount
 
-@dataclass
-class MsrpChain:
-    current: MsrpSeasonPart
-    next: "MsrpChain" | None = None
+    def get_total_value_in_range(self) -> float:
+        return self.get_total_msrp() * self.effective_weight
 
+    def get_total_paid_in_range(self) -> float:
+        return self.monthly_due_effective * self.effective_months
 
-def already_paid_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    return current_msrp.msrp_eff * (current_msrp.months - new_msrp.months)
+    def set_effective_months(self, months: int):
+        self.effective_months = months
 
+    def set_effective_weight(self, weight: float):
+        self.effective_weight = weight
 
-def pay_eff_expected_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    duration_factor = (current_msrp.months - new_msrp.months) / current_msrp.months
-    weight_factor = (current_msrp.weight - new_msrp.weight) / current_msrp.weight
-    return (
-        current_msrp.msrp_eff
-        * weight_factor
-        / duration_factor
-        * (current_msrp.months - new_msrp.months)
-    )
-
-
-def overpay_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    return already_paid_total(current_msrp, new_msrp) - pay_eff_expected_total(
-        current_msrp, new_msrp
-    )
-
-
-def new_pay_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    return new_msrp.msrp * current_msrp.months * new_msrp.weight
-
-
-def new_pay_eff_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    return new_pay_total(current_msrp, new_msrp) - overpay_total(current_msrp, new_msrp)
-
-
-def total_pay(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
-    return (
-        current_msrp.msrp_eff * (current_msrp.months - new_msrp.months)
-        + new_msrp.msrp_eff * new_msrp.months
-    )
+    def set_overpay(self, overpay: float):
+        self.overpay = overpay
 
 
 def calc_msrp(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart):
+    def already_paid_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        return current_msrp.msrp_eff * (current_msrp.months - new_msrp.months)
+
+    def pay_eff_expected_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        duration_factor = (current_msrp.months - new_msrp.months) / current_msrp.months
+        weight_factor = (current_msrp.weight - new_msrp.weight) / current_msrp.weight
+        return (
+            current_msrp.msrp_eff
+            * weight_factor
+            / duration_factor
+            * (current_msrp.months - new_msrp.months)
+        )
+
+    def overpay_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        return already_paid_total(current_msrp, new_msrp) - pay_eff_expected_total(
+            current_msrp, new_msrp
+        )
+
+    def new_pay_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        return new_msrp.msrp * current_msrp.months * new_msrp.weight
+
+    def new_pay_eff_total(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        return new_pay_total(current_msrp, new_msrp) - overpay_total(current_msrp, new_msrp)
+
+    def total_pay(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart) -> float:
+        return (
+            current_msrp.msrp_eff * (current_msrp.months - new_msrp.months)
+            + new_msrp.msrp_eff * new_msrp.months
+        )
 
     print(f"already paid: {already_paid_total(current_msrp, new_msrp):.2f}")
     print(f"pay_eff_expected: {pay_eff_expected_total(current_msrp, new_msrp):.2f}")
@@ -78,19 +87,66 @@ def calc_msrp(current_msrp: MsrpSeasonPart, new_msrp: MsrpSeasonPart):
     print(f"total pay: {total_pay(current_msrp, new_msrp):.2f}")
 
 
-def calc_msrp_chain(chain: MsrpChain):
+def calc_msrp_chain(chain: list[MsrpSeasonPart]):
     yearly_msrp = 0
-    while True:
-        print(f"current: {chain.current}")
-        print(f"next: {chain.next.current if chain.next else None}")
-        current = chain.current
-        next_weight = chain.next.current.weight if chain.next else 0
-        yearly_msrp += current.get_total_msrp() * (current.weight - next_weight)
+    previous = None
+    for i in range(len(chain)):
+        current = chain[i]
+        next = chain[i + 1] if i + 1 < len(chain) else None
+        previous = chain[i - 1] if i - 1 >= 0 else None
+
+        current.set_effective_months(current.months - (next.months if next else 0))
+        current.set_effective_weight(current.weight - (next.weight if next else 0))
+
+        print(f"\ncurrent: {current}")
+        print(f"next:    {next}")
+
+        # calculate product value for current range
+        yearly_msrp += current.get_total_value_in_range()
         print(f"--> yearly_msrp: {yearly_msrp:.2f}")
-        chain = chain.next
-        if not chain:
-            break
-    print(f"yearly_msrp: {yearly_msrp:.2f}")
+
+    previous = None
+    for i in range(len(chain)):
+        current = chain[i]
+        next = chain[i + 1] if i + 1 < len(chain) else None
+        previous = chain[i - 1] if i - 1 >= 0 else None
+
+        print(f"\ncurrent: {current}")
+        print(f"next:    {next}")
+
+        # calculate paid value for current range
+        # next_months = chain.next.current.months if chain.next else 0
+        remaining_value = current.get_total_msrp() * current.weight
+        monthly_due = remaining_value / current.months
+
+        previous_overpay = 0
+        if previous:
+            previous_overpay = (
+                previous.monthly_due_effective * (previous.months - current.months)
+                - previous.get_total_value_in_range()
+            )
+            previous.set_overpay(previous_overpay)
+
+        current.set_monthly_due_effective(monthly_due - previous_overpay / current.months)
+
+        print(f"--> remaining_value: {remaining_value:.2f}")
+        print(f"--> monthly_due: {monthly_due:.2f}")
+        print(f"--> previous_overpay: {previous_overpay:.2f}")
+        print(f"--> monthly_due_effective: {current.monthly_due_effective:.2f}")
+
+        # paid_at_end_of_season = current.monthly_due_effective * current.months
+
+    print(f"\n------\nyearly_msrp: {yearly_msrp:.2f}")
+
+    last_month = 0
+    cumulative_sum = 0
+    for part in chain:
+        for __ in range(part.effective_months):
+            cumulative_sum += part.monthly_due_effective
+            print(
+                f"month {last_month+1:02d}: {part.monthly_due_effective:.2f} (cumulative: {cumulative_sum:.2f})"
+            )
+            last_month += 1
 
 
 if __name__ == "__main__":
@@ -107,15 +163,15 @@ if __name__ == "__main__":
     #     calc_msrp(current_msrp, new_msrp)
 
     # more than one change
-    changes = [
-        MsrpSeasonPart(product=product, amount=1, msrp_eff=100, months=12, weight=1),
-        MsrpSeasonPart(product=product, amount=1, msrp_eff=None, months=6, weight=0.8),
-        MsrpSeasonPart(product=product, amount=0, msrp_eff=None, months=3, weight=0.2),
+    chain = [
+        MsrpSeasonPart(product=product, amount=1, months=12, weight=1),
+        MsrpSeasonPart(product=product, amount=1, months=6, weight=0.7),
+        MsrpSeasonPart(product=product, amount=1, months=3, weight=0.2),
     ]
-    chain = MsrpChain(
-        current=changes[0],
-        next=MsrpChain(current=changes[1], next=MsrpChain(current=changes[2], next=None)),
-    )
+    # chain = MsrpChain(
+    #     current=changes[0],
+    #     next=MsrpChain(current=changes[1], next=MsrpChain(current=changes[2], next=None)),
+    # )
 
     print("\n\nmore than one change")
     print(f"chain: {chain}")
