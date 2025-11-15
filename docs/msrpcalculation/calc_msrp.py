085456d58631ba09feafaeccd2d940cfb310603a
joergrs <joergschroeter84@gmail.com>
2025-11-15 13:52:33 +0100

    clean implementation #302

--- docs/msrpcalculation/calc_msrp.py
+++ docs/msrpcalculation/calc_msrp.py
@@ -21,16 +21,20 @@ class MsrpSeasonPart:
     monthly_due_effective: float | None = None
     effective_months: int | None = None
     effective_weight: float | None = None
-    overpay: float = 0
 
     def get_total_msrp(self) -> float:
+        # value of the product if delivered in full amount
         return self.product.base_msrp * self.product.frequency * self.amount
 
-    def set_monthly_due_effective(self, amount: float):
-        self.monthly_due_effective = amount
+    def get_total_remaining_value(self) -> float:
+        # value of the product if delivered until the end of the season
+        return self.get_total_msrp() * self.weight
 
     def get_total_value_in_range(self) -> float:
         return self.get_total_msrp() * self.effective_weight
+
+    def set_monthly_due_effective(self, amount: float):
+        self.monthly_due_effective = amount
 
     def get_total_paid_in_range(self) -> float:
         return self.monthly_due_effective * self.effective_months
@@ -41,13 +45,11 @@ class MsrpSeasonPart:
     def set_effective_weight(self, weight: float):
         self.effective_weight = weight
 
-    def set_overpay(self, overpay: float):
-        self.overpay = overpay
-
 
 def calc_msrp_chain(chain: list[MsrpSeasonPart]):
-    yearly_msrp = 0
-    yearly_msrps = [0]
+
+    cumulative_yearly_msrp = 0
+    cumulative_paid = 0
     for i in range(len(chain)):
         current = chain[i]
         next = chain[i + 1] if i + 1 < len(chain) else None
@@ -58,37 +60,21 @@ def calc_msrp_chain(chain: list[MsrpSeasonPart]):
         print(f"\ncurrent: {current}")
         print(f"next:    {next}")
 
-        # calculate product value for current range
-        yearly_msrp += current.get_total_value_in_range()
-        yearly_msrps.append(yearly_msrp)
-        print(f"--> yearly_msrp: {yearly_msrp:.2f}")
+        remaining_value = current.get_total_remaining_value()
 
-    cumulative_paid = 0
-    for i in range(len(chain)):
-        current = chain[i]
-        next = chain[i + 1] if i + 1 < len(chain) else None
-
-        print(f"\ncurrent: {current}")
-        print(f"next:    {next}")
-
-        # calculate paid value for current range
-        # next_months = chain.next.current.months if chain.next else 0
-        remaining_value = current.get_total_msrp() * current.weight
-        monthly_due = remaining_value / current.months
-
-        relevant_yearly_msrp = yearly_msrps[i] + remaining_value
+        relevant_yearly_msrp = cumulative_yearly_msrp + remaining_value
 
         current.set_monthly_due_effective((relevant_yearly_msrp - cumulative_paid) / current.months)
         cumulative_paid += current.get_total_paid_in_range()
 
+        cumulative_yearly_msrp += current.get_total_value_in_range()
+
+        print(f"--> yearly_msrp: {cumulative_yearly_msrp:.2f}")
         print(f"--> remaining_value: {remaining_value:.2f}")
-        print(f"--> monthly_due: {monthly_due:.2f}")
         print(f"--> cumulative_paid: {cumulative_paid:.2f}")
         print(f"--> monthly_due_effective: {current.monthly_due_effective:.2f}")
 
-        # paid_at_end_of_season = current.monthly_due_effective * current.months
-
-    print(f"\n------\nyearly_msrp: {yearly_msrp:.2f}")
+    print(f"\n------\nyearly_msrp: {cumulative_yearly_msrp:.2f}")
 
     last_month = 0
     cumulative_sum = 0
@@ -100,7 +86,7 @@ def calc_msrp_chain(chain: list[MsrpSeasonPart]):
             )
             last_month += 1
 
-    assert round(cumulative_sum, 2) == round(yearly_msrp, 2)
+    assert round(cumulative_sum, 2) == round(cumulative_yearly_msrp, 2)
 
 
 if __name__ == "__main__":
@@ -110,11 +96,13 @@ if __name__ == "__main__":
     # more than one change
     chain = [
         MsrpSeasonPart(product=product, amount=1, months=12, weight=1),
-        MsrpSeasonPart(product=product, amount=1, months=7, weight=0.7),
-        MsrpSeasonPart(product=product, amount=2, months=5, weight=0.2),
+        MsrpSeasonPart(product=product, amount=0.5, months=7, weight=0.7),
+        MsrpSeasonPart(product=product, amount=4.5, months=5, weight=0.2),
         MsrpSeasonPart(product=product, amount=0, months=4, weight=0.1),
     ]
 
     print("\n\nmore than one change")
     print(f"chain: {chain}")
-    calc_msrp_chain(chain)
+
+    for i in range(len(chain)):
+        calc_msrp_chain(chain[: i + 1])
